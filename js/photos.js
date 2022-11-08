5befc339d17175459533261baacd8fe219e9ada9
Antor-Kumar <101360719+Antor-Kumar@users.noreply.github.com>
2022-11-08 17:54:10 +0600

    Add files via upload

---
+++ js/photos.js
@@ -0,0 +1,26 @@
+function handlePhotoclick() {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((res) => res.json())
+      .then((data) => displayComments(data));
+  }
+  
+  function displayComments(photos) {
+    const commentContainer = document.getElementById("photos-container");
+    commentContainer.classList.add('photos')
+    for (const photo of photos.slice(0,10)) {
+      const div = document.createElement("div");
+      div.classList.add('photo')
+      const h2 = document.createElement("h2");
+      div.appendChild(h2);
+      const p = document.createElement("p");
+      div.appendChild(p);
+      const img = document.createElement("img");
+      img.classList.add('img-width');
+      div.appendChild(img);
+      img.src = photo.url;
+      div.innerHTML = `<h2>${photo.title}</h2>`
+      commentContainer.appendChild(img);
+      // commentContainer.appendChild(div);
+    }
+  }
+  
